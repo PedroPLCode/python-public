1678ea4cf474f07dc74363db62c911b5838e57a4
pedro <125091008+PedroPLCode@users.noreply.github.com>
2023-02-26 20:14:21 +0100

    Add files via upload

---
+++ currency_exchange.py
@@ -0,0 +1,198 @@
+"""Currency Exchange Calculator
+Author: Pedro
+Last update: 23.02.2023
+
+One of my self learning, small projects.
+First version had 420 lines and was using only variables, 
+current version using dictionary, still trying to improve.
+Any comments welcome :)
+"""
+
+currency_dict = {
+    'pln': {'pln / eur': 4.76,
+            'pln / gbp': 5.36,
+            'pln / usd': 4.45, },
+    'eur': {'eur / pln': 0.21,
+            'eur / usd': 1.07,
+            'eur / gbp': 0.89, },
+    'gbp': {'gbp / pln': 0.19,
+            'gbp / eur': 1.13,
+            'gbp / usd': 1.20},
+    'usd': {'usd / pln': 0.22,
+            'usd / eur': 1.07,
+            'usd / gbp': 0.83, },
+}
+
+
+def main():
+    """Main menu to choose currency to work with."""
+    print("\n-----------------------------------------\n"
+          "-----------Currency Exchange-------------\n"
+          "---------------Calculator----------------\n"
+          "---------------------------------pedro-v4\n"
+          "\nHello. What currency will we exchange?\n")
+    show_options(currency_dict)
+    counter = 0
+    while True:
+        currency = str(input("\nEnter choosen currency or Q to Quit: "))
+        currency = currency.lower().strip()
+
+        if currency in get_options(currency_dict):
+
+            next = True
+
+            show_currencies(currency, currency_dict)
+            change_currencies(currency, currency_dict)
+            show_currencies(currency, currency_dict)
+
+            while (next):
+                currnecy_exchange(currency, currency_dict)
+
+                while True:
+
+                    next = str(input(
+                        f"\nDo you want to exchange another {currency.upper()} amount? "
+                        "Answer y / n "))
+                    next = next.lower().strip()
+
+                    if next == "y" or next == "yes":
+                        print("Ok. Let's count again.")
+                        break
+
+                    elif next == "n" or next == 'no':
+                        print("\nOk. Do you want to count another currency?\n")
+                        show_options(currency_dict)
+                        next = False
+                        break
+
+                    else:
+                        print("Wrong answer. Please answer y / n")
+
+        elif currency == "q" or currency == "quit" or currency == "n" or currency == "no":
+            print("Ok. Quit.")
+            break
+
+        else:
+            print("Wrong answer.")
+            counter = counter + 1
+            if counter == 3:
+                print("")
+                show_options(currency_dict)
+                counter = 0
+
+
+def show_currencies(currency, dict):
+    """"Shows actual currencies for currency, taken from currencies dictionary"""
+    print("\nActual currencies:")
+    for key, values in dict.items():
+        for key_curr, val_curr in values.items():
+            if currency == key:
+                print(f"Currency {key_curr.upper()}: {round(val_curr, 2)}")
+
+
+def change_currencies(currency, dict):
+    """Allows to change choosen currency in dictionary"""
+    for key, values in dict.items():
+        for key_curr, val_curr in values.items():
+            if currency == key:
+
+                while True:
+                    change = str(input(f"\nDo you want to change {key_curr.upper()} currency? "
+                                       "Answer y / n  "))
+                    change = change.lower().strip()
+
+                    if change == "y" or change == "yes":
+                        new = input(f"Enter new {key_curr.upper()} currency: ")
+                        new = convert_input_to_float(new)
+
+                        if check_input_type(new):
+                            continue
+                        else:
+                            new = float(new)
+
+                        values[key_curr] = round(new, 2)
+                        print(
+                            f"Currency {key_curr.upper()} changed to {str(round(new, 2))}")
+                        break
+
+                    elif change == "n" or change == "no":
+                        print(f"Currency {key_curr.upper()} not changed.")
+                        break
+
+                    else:
+                        print(
+                            f"Wrong answer. Currency {key_curr.upper()} not changed.")
+
+
+def currnecy_exchange(currency, dict):
+    """Exchanges currencies and shows results."""
+    while True:
+        amount = input(f"\nEnter {(currency.upper())} amount to exchange: ")
+        amount = convert_input_to_float(amount)
+
+        if check_input_type(amount):
+            continue
+        else:
+            amount = float(amount)
+            break
+
+    print("\nExchange results:")
+
+    for key, values in dict.items():
+        for key_curr, val_curr in values.items():
+            if currency == key:
+                result = float(amount) / float(val_curr)
+                if result < 0.01:
+                    result = str("less than 0.01")
+                else:
+                    result = round(result, 2)
+
+                print(
+                    f"{str(round(amount, 2))} {(currency.upper())} "
+                    f"equals: {str(result)} {key_curr[-3:].upper()}")
+
+
+def get_options(dict):
+    """Gets possible currency options from currencies dictionary and returns as list"""
+    options = []
+    for key in dict.keys():
+        options.append(key)
+    return options
+
+
+def show_options(dict):
+    """Shows possible currency options from currencies dictionary"""
+    print("Options:")
+    for key in dict.keys():
+        print(key.upper())
+
+
+def check_input_type(input):
+    try:
+        input = float(input)
+        return False
+    except ValueError:
+        try:
+            input = int(input)
+            return False
+        except ValueError:
+            print(f"\nInput {input} is not a correct number. Looks like string."
+                   "Please try again.")
+            return True
+
+
+def convert_input_to_float(input):
+    """Converts entered decimals with ',' to floats with '.' if possible."""
+    if ',' in input:
+        splitted = input.split(',')
+
+        if len(splitted) == 2:
+            converted = (splitted[0]+'.'+splitted[1])
+            converted = float(converted)
+            return converted
+
+    else:
+        return input
+
+
+main()
---
+++ simple_calculator.py
@@ -0,0 +1,104 @@
+"""Simple Calculator
+Author: Pedro
+Last update: 23.02.2023
+Any comments welcome :)
+"""
+
+def main():
+    
+    print("\n--------Welcome--------\n"
+          "---Simple-calculator---\n"
+          "---------------pedro-v3")
+
+    print_info()
+
+    while count_again():
+
+        user_string = input("Enter operation: ")
+        list = user_string.split()
+
+        number1 = convert_input_to_float(list[0])
+        number2 = convert_input_to_float(list[2])
+        operation = list[1]
+
+        if check_input_type(number1) or check_input_type(number2):
+            continue
+
+        number1 = float(number1)
+        number2 = float(number2)
+        operation = str(operation)
+
+        print(f"Result: {calculate(number1, operation, number2)}\n")
+
+
+def count_again():
+    """Checks if user wants to count or quit. Returns True or False"""
+    while True:
+        answer = input("Continue? Enter y to count or q to quit? ")
+        answer = answer.strip().lower()
+        if answer == "q" or answer == "quit":
+            print("Ok. Quit")
+            return False
+            break
+        elif answer == "y" or answer == "yes":
+            print("\nOk. Lets count.\n")
+            return True
+            break
+        else:
+            print("Wrong answer.")
+
+
+def calculate(number1, operation, number2):
+    """Making a calculation ,depending on operation input. Returns result"""
+    if operation == "+":
+        result = number1 + number2
+    elif operation == "-":
+        result = number1 - number2
+    elif operation == "*":
+        result = number1 * number2
+    elif operation == "/":
+        result = number1 / number2
+    elif operation == "%":
+        result = number1 % number2
+    else:
+        result = str("Wrong. Operation not supported.")
+        print_info()
+    return result
+
+
+def check_input_type(input):
+    """Checks if input is float, int or string."""
+    try:
+        input = float(input)
+        return False
+    except ValueError:
+        try:
+            input = int(input)
+            return False
+        except ValueError:
+            print(f"\nInput {input} is not a correct number. Looks like string.\n")
+            return True
+
+
+def convert_input_to_float(input):
+    """Converts entered decimals with ',' to floats with '.' if possible."""
+    if ',' in input:
+        splitted = input.split(',')
+
+        if len(splitted) == 2:
+            converted = (splitted[0]+'.'+splitted[1])
+            converted = float(converted)
+            return converted
+
+    else:
+        return input
+
+
+def print_info():
+    """Prints simple info of supported operations."""
+    print("\nEnter first number, operation and second number."
+          "\ndivided by space (for example 2 + 3)"
+          "\nsupperted operations: + - / * %\n")
+
+
+main()
