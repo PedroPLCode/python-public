50d445511d0e443d21d49970d39b27d10b50808a
PedroPLCode <piotrek.gaszczynski@gmail.com>
2023-03-08 12:13:42 +0100

    upgrades

--- unicorns_forever/unicorn.py
+++ unicorns_forever/unicorn.py
@@ -7,12 +7,28 @@ class Unicorn:
         """Unicorn initialization."""
 
         self.screen = uf_game.screen
+        self.settings = uf_game.settings
         self.screen_rect = uf_game.screen.get_rect()
 
         self.image = pygame.image.load('/home/pedro/Dokumenty/python-public/unicorns_forever/images/unicorn.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Unicorn location updating."""
+
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.settings.unicorn_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.settings.unicorn_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         """Shows unicorn in current location."""
--- unicorns_forever/unicorns_forever.py
+++ unicorns_forever/unicorns_forever.py
@@ -11,8 +11,10 @@ class UnicornsForever:
         """Game initialization."""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Unicorns Forever")
         self.unicorn = Unicorn(self)
 
@@ -20,6 +22,7 @@ class UnicornsForever:
         """Game main loop."""
         while True:
             self._check_events()
+            self.unicorn.update()
             self._update_screen()
 
     def _check_events(self):
@@ -27,12 +30,33 @@ class UnicornsForever:
         for event in pygame.event.get(): # naciśnięcie klawisza lub myszy
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_events(event)     
+
+    def _check_keydown_events(self, event):
+        """Key down reaction."""
+        if event.key == pygame.K_RIGHT:
+            self.unicorn.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.unicorn.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Key up reaction."""
+        if event.key == pygame.K_RIGHT:
+            self.unicorn.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.unicorn.moving_left = False
+
 
     def _update_screen(self):
-         """Updating screen view."""
-         self.screen.fill(self.settings.bg_color) # kolor tła , zrobic inny
-         self.unicorn.blitme()
-         pygame.display.flip() # ostatni ekran
+        """Updating screen view."""
+        self.screen.fill(self.settings.bg_color) # kolor tła , zrobic inny
+        self.unicorn.blitme()
+        pygame.display.flip() # ostatni ekran
 
 if __name__=='__main__':
     uf = UnicornsForever()
