5793a9a86402a7a4a499c84c6f6bd695f1f45937
PedroPLCode <piotrek.gaszczynski@gmail.com>
2023-03-14 18:34:03 +0100

    last upgrades

--- unicorns_forever/settings.py
+++ unicorns_forever/settings.py
@@ -27,7 +27,7 @@ class Settings:
         self.hord_drop_speed = 10
         self.trolls_starts_shoot_max = 12
         self.trolls_starts_shoot_min = 6
-        self.trolls_starts_shoot = randint(self.trolls_starts_shoot_min, self.trolls_starts_shoot_max)
+        self.when_trolls_starts_shoot = randint(self.trolls_starts_shoot_min, self.trolls_starts_shoot_max)
 
         self.speed_up_scale = 1.1
         self.score_scale = 1.5
